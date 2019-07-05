c13239c691b60b4eea2e041afbc48f277ce7426f
uzaisindiko <uzai.sindiko@gmail.com>
2019-07-05 14:26:10 +0700

    [Exercises 12] Konversi Menit

---
+++ exercise-12.js
@@ -0,0 +1,23 @@
+function konversiMenit(menit) {
+	var hour = 0;
+	for (var i = menit; i >= 60; i -= 60) {
+		hour++;
+	}
+
+	// var hour = Math.floor(menit / 60);
+
+	var minute = menit % 60;
+	if (minute < 10) {
+		return hour + ':0' + minute;
+	}
+	else {
+		return hour + ':' + minute;
+	}
+}
+
+// TEST CASES
+console.log(konversiMenit(63)); // 1:03
+console.log(konversiMenit(124)); // 2:04
+console.log(konversiMenit(53)); // 0:53
+console.log(konversiMenit(88)); // 1:28
+console.log(konversiMenit(120)); // 2:00
